90b5b90631cdcd5457ef7af1dde662e86c71dc18
SergeiKrutii <106181150+SergeiKrutii@users.noreply.github.com>
2023-01-27 00:23:14 +0200

    rework with async/await

--- src/api.js/fetchCountries.js
+++ src/api.js/fetchCountries.js
@@ -1,15 +1,26 @@
 const BASE_URL = 'https://restcountries.com/v2/name/';
 const filterRespons = 'name,capital,population,flags,languages';
 
-const fetchCountries = country => {
-  return fetch(`${BASE_URL}${country}?fields=${filterRespons}`).then(
-    responce => {
-      if (responce.status === 404) {
-        return Promise.reject(new Error('Enter country!!'));
-      }
-      return responce.json();
-    }
-  );
+
+const fetchCountries = async (country) => {
+  const res = await fetch(`${BASE_URL}${country}?fields=${filterRespons}`);
+  if (res.status === 404)  {
+    return Promise.reject(new Error('Enter country!!'));
+  }
+  return res.json();
 };
+
+
+
+// const fetchCountries = country => {
+//   return fetch(`${BASE_URL}${country}?fields=${filterRespons}`).then(
+//     responce => {
+//       if (responce.status === 404) {
+//         return Promise.reject(new Error('Enter country!!'));
+//       }
+//       return responce.json();
+//     }
+//   );
+// };
 
 export { fetchCountries };
--- src/index.js
+++ src/index.js
@@ -2,17 +2,14 @@ import './css/styles.css';
 import { fetchCountries } from './api.js/fetchCountries';
 import { refs } from './api.js/refs';
 import { Notify } from '../node_modules/notiflix/build/notiflix-notify-aio';
-import {
-  createMarkupForAll,
-  createMarkupForOne,
-} from './api.js/data-markup';
+import { createMarkupForAll, createMarkupForOne } from './api.js/data-markup';
 var debounce = require('lodash.debounce');
 
 const DEBOUNCE_DELAY = 300;
 
 refs.input.addEventListener('input', debounce(findCountry, DEBOUNCE_DELAY));
 
-function findCountry(e) {
+async function findCountry(e) {
   e.preventDefault();
   const inputValue = refs.input.value.trim();
   if (inputValue.length === 0) {
@@ -20,26 +17,47 @@ function findCountry(e) {
     return;
   }
 
-  fetchCountries(inputValue)
-    .then(countries => {
-      if (countries.length === 1) {
-        let markupforOne = createMarkupForOne(countries);
-        refs.div.innerHTML = markupforOne;
-      } else {
-        Notify.info(
-          'Too many matches found... Please enter a more specific name.'
-        );
-        refs.div.innerHTML = '';
-      }
-      if (countries.length >= 2 && countries.length <= 10) {
-        let markupForAll = createMarkupForAll(countries);
-        refs.div.innerHTML = markupForAll;
-      }
-    })
-    .catch(err => {
-      if (err.message) {
-        Notify.failure('Oops, there is no country with that name');
-        refs.div.innerHTML = '';
-      }
-    });
+  try {
+    const countries = await fetchCountries(inputValue);
+    console.log(countries);
+    if (countries.length === 1) {
+      let markupforOne = createMarkupForOne(countries);
+      refs.div.innerHTML = markupforOne;
+    } else {
+      Notify.info(
+        'Too many matches found... Please enter a more specific name.'
+      );
+      refs.div.innerHTML = '';
+    }
+    if (countries.length >= 2 && countries.length <= 10) {
+      let markupForAll = createMarkupForAll(countries);
+      refs.div.innerHTML = markupForAll;
+    }
+  } catch (error) {
+    Notify.failure('Oops, there is no country with that name');
+    refs.div.innerHTML = '';
+  }
 }
+
+//   fetchCountries(inputValue)
+//     .then(countries => {
+// if (countries.length === 1) {
+//   let markupforOne = createMarkupForOne(countries);
+//   refs.div.innerHTML = markupforOne;
+// } else {
+//   Notify.info(
+//     'Too many matches found... Please enter a more specific name.'
+//   );
+//   refs.div.innerHTML = '';
+//       }
+//       if (countries.length >= 2 && countries.length <= 10) {
+//         let markupForAll = createMarkupForAll(countries);
+//         refs.div.innerHTML = markupForAll;
+//       }
+//     })
+//     .catch(err => {
+// if (err.message) {
+//   Notify.failure('Oops, there is no country with that name');
+//   refs.div.innerHTML = '';
+// }
+//     });
